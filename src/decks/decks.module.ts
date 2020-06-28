7e6fe16e67636e65312a18f53ce453b7e8317c06
Kevin Cipolla <k2cipolla@gmail.com>
2020-06-28 07:04:55 -0400

    fixed some issues with module imports

--- src/decks/decks.module.ts
+++ src/decks/decks.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import DecksController from './decks.controller';
 import DecksService from './decks.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Deck } from './deck.entity';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Deck])],
   controllers: [DecksController],
   providers: [DecksService]
 })
